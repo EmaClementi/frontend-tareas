5577dcc5cc343cfbefb70f01d793ca345def4795
EmaClementi <emicabj4221@gmail.com>
2026-02-22 10:48:23 -0300

    configurando deploy 2

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "tareas-production-a471.up.railway.app",
+  baseURL: "https://tareas-production-a471.up.railway.app/",
 });
 
 // Interceptor de REQUEST - Agregar token automáticamente
