ca125b3fa3ec1859bb17a37f14fb96efa3480758
EmaClementi <emicabj4221@gmail.com>
2026-03-22 14:37:18 -0300

    modificacion endpoint

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tareas-2-i3b4.onrender.com/",
+  baseURL: "https://tareas-2-i3b4.onrender.com",
 });
 
 // Interceptor de REQUEST - Agregar token automáticamente
