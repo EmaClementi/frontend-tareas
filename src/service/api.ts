5dd48eba020463cf662d52f74e5c4c60c7dc79d7
EmaClementi <emicabj4221@gmail.com>
2026-04-26 17:59:03 -0300

    modificacion url api backend

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://168.75.96.142:8080",
+  baseURL: "https://corps-format-guidance-strategic.trycloudflare.com",
 });
 
 // Interceptor de REQUEST - Agregar token automáticamente
